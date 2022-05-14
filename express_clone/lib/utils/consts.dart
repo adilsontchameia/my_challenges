ac8ff95379e0348464f135c21f9b5f649cfd1366
adilsonchameia <adilsonkchameia@outlook.com>
2022-05-14 19:07:32 +0100

    criacao do numPad e consts

---
+++ express_clone/lib/utils/consts.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const splashColor = Color(0xFFF79032);
+const backGroundColor = Color(0xFFFEFEFE);
+const emptyPinCircle = Color(0xFFA2A2A2);
+const enteredPinCircle = Colors.black;
+const numPadColor = Color(0xFF757676);
+const titlePadColor = Color(0xFF919190);
+const iconAndHeadMainColor = Color(0xFFF58300);
