e2b5c6cbf69104b420a36b6a67dcccbfc261494e
adilsonchameia <adilsonkchameia@outlook.com>
2022-09-06 22:40:15 +0100

    drawer

--- vunongue_tour/lib/widgets/drawer/custom_drawer.dart
+++ vunongue_tour/lib/widgets/drawer/custom_drawer.dart
@@ -5,6 +5,43 @@ class CustomDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Drawer();
+    return Drawer(
+      child: Column(
+        children: [
+          Container(
+            height: 300,
+            color: Colors.red,
+          ),
+          const SizedBox(height: 10.0),
+          DrawerTile(icon: Icons.home, text: 'Home'),
+          const SizedBox(height: 15.0),
+          DrawerTile(icon: Icons.home, text: 'Home'),
+          const SizedBox(height: 15.0),
+          DrawerTile(icon: Icons.home, text: 'Home'),
+        ],
+      ),
+    );
+  }
+}
+
+class DrawerTile extends StatelessWidget {
+  DrawerTile({Key? key, required this.icon, required this.text})
+      : super(key: key);
+  IconData icon;
+  String text;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 32),
+          child: Icon(icon, size: 32, color: Colors.grey),
+        ),
+        Text(
+          text,
+          style: const TextStyle(fontSize: 16, color: Colors.grey),
+        )
+      ],
+    );
   }
 }
