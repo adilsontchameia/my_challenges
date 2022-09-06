e5b92bcb1e8ee0be8f0eececbc603de11d2cd4c7
adilsonchameia <adilsonkchameia@outlook.com>
2022-09-06 22:57:52 +0100

    drawer

--- vunongue_tour/lib/widgets/drawer/custom_drawer.dart
+++ vunongue_tour/lib/widgets/drawer/custom_drawer.dart
@@ -6,19 +6,37 @@ class CustomDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Column(
-        children: [
-          Container(
-            height: 300,
-            color: Colors.red,
-          ),
-          const SizedBox(height: 10.0),
-          DrawerTile(icon: Icons.home, text: 'Home'),
-          const SizedBox(height: 15.0),
-          DrawerTile(icon: Icons.home, text: 'Home'),
-          const SizedBox(height: 15.0),
-          DrawerTile(icon: Icons.home, text: 'Home'),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+                height: 300,
+                color: Colors.red,
+                child: AspectRatio(
+                    aspectRatio: 1.5,
+                    child: Image.asset(
+                      'assets/onboard-image.jpg',
+                      fit: BoxFit.fitWidth,
+                    ))),
+            const SizedBox(height: 10.0),
+            DrawerTile(icon: Icons.home, text: 'Inicio'),
+            const SizedBox(height: 15.0),
+            DrawerTile(icon: Icons.favorite, text: 'Favoritos'),
+            const SizedBox(height: 15.0),
+            DrawerTile(icon: Icons.local_activity, text: 'Atividades'),
+            const SizedBox(height: 50.0),
+            DrawerTile(icon: Icons.home, text: 'Inicio'),
+            const SizedBox(height: 15.0),
+            DrawerTile(icon: Icons.person, text: 'Log-In'),
+            const SizedBox(height: 120.0),
+            const Text(
+              'developed by: Adilson Tchameia',
+              style: TextStyle(fontSize: 10.0, color: Colors.grey),
+            ),
+            const Text('stocks provided by: CSK Studio',
+                style: TextStyle(fontSize: 10.0, color: Colors.grey)),
+          ],
+        ),
       ),
     );
   }
