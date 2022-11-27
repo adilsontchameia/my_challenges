9b3a77ad0c3109e2e36b8e9976a509e75d6b96f2
Adilson Tchameia <adilsonkchameia@outlook.com>
2022-11-28 00:11:11 +0100

    created the banner

--- ecommerce_ui/lib/pages/home/bottom_nav_pages/resume_page.dart
+++ ecommerce_ui/lib/pages/home/bottom_nav_pages/resume_page.dart
@@ -61,6 +61,58 @@ class ResumePage extends StatelessWidget {
                   color: Colors.red,
                   borderRadius: BorderRadiusDirectional.circular(10.0),
                 ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Image.asset(
+                        'assets/headphone_blue2.png',
+                        height: 170,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'See the the products !',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(height: 5.0),
+                          Text(
+                            'This is a text that serves as example.',
+                            softWrap: true,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              color: Colors.white.withOpacity(0.8),
+                              fontSize: 12.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 5.0),
+                          Container(
+                            height: 30.0,
+                            width: 90.0,
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(5.0)),
+                            child: const Center(
+                              child: Text(
+                                'Explore',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 13.5,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
               ),
             ),
             const SizedBox(height: 20),
