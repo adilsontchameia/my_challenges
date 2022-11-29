d64e5df97be114e11f6260c6624d5cdd0f2d12b6
Adilson Tchameia <adilsonkchameia@outlook.com>
2022-11-29 14:03:04 +0100

    created more components at detail screen

--- ecommerce_ui/lib/src/pages/product_details/product_details.dart
+++ ecommerce_ui/lib/src/pages/product_details/product_details.dart
@@ -7,114 +7,178 @@ class ProductDetailsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Stack(
-            children: [
-              Container(
-                height: 330.0,
-                width: double.infinity,
-                color: Colors.red,
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 20.0),
-                  child: Image.asset('assets/headphone_blue.png'),
-                ),
-              ),
-              SafeArea(
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
-                      Icon(Icons.arrow_back_rounded),
-                      FavouriteButton(),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 5.0),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
               children: [
-                const Text(
-                  'HeadPhones JBL XY20 Sky Blue',
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold,
+                Container(
+                  height: 330.0,
+                  width: double.infinity,
+                  color:
+                      const Color.fromARGB(255, 224, 238, 255).withOpacity(0.7),
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 30.0),
+                    child: Image.asset('assets/headphone_blue.png'),
                   ),
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    Text(
-                      'By JBL Corp',
-                      style: TextStyle(
-                        fontSize: 15.0,
-                        fontWeight: FontWeight.w500,
-                      ),
+                SafeArea(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Icon(Icons.arrow_back_rounded),
+                        FavouriteButton(),
+                      ],
                     ),
-                    Icon(
-                      Icons.check_box_rounded,
-                      color: Colors.blue,
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 15.0),
-                const Text(
-                  'Select QTY',
-                  style: TextStyle(
-                    fontSize: 15.0,
-                    fontWeight: FontWeight.bold,
                   ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      height: 30,
-                      width: 95,
-                      decoration: BoxDecoration(
-                        color: Colors.grey.shade200,
-                        borderRadius: BorderRadius.circular(8.0),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: const [
-                          Icon(
-                            Icons.add,
-                            color: Colors.black,
-                          ),
-                          Text(
-                            '1',
-                            style: TextStyle(
-                              fontSize: 15.0,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Icon(
-                            Icons.add,
-                            color: Colors.black,
-                          ),
-                        ],
-                      ),
-                    ),
-                    const Text(
-                      '\$100,35',
-                      style: TextStyle(
-                        fontSize: 19.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
                 ),
               ],
             ),
+            const SizedBox(height: 5.0),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'HeadPhones JBL XY20 Sky Blue',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 5.0),
+                  Row(
+                    children: const [
+                      Text(
+                        'By JBL Corp',
+                        style: TextStyle(
+                          fontSize: 15.0,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      SizedBox(width: 5.0),
+                      Icon(
+                        Icons.check_box_rounded,
+                        size: 18,
+                        color: Colors.blue,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 18.0),
+                  const Text(
+                    'Select QTY',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        height: 30,
+                        width: 95,
+                        decoration: BoxDecoration(
+                          color: Colors.grey.shade200,
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: const [
+                            Icon(
+                              Icons.add,
+                              color: Colors.black,
+                            ),
+                            Text(
+                              '1',
+                              style: TextStyle(
+                                fontSize: 15.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Icon(
+                              Icons.add,
+                              color: Colors.black,
+                            ),
+                          ],
+                        ),
+                      ),
+                      const Text(
+                        '\$100,35',
+                        style: TextStyle(
+                          fontSize: 19.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 35),
+                  const Text(
+                    'Product Details',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 5.0),
+                  const Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.',
+                    textAlign: TextAlign.justify,
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  const SizedBox(height: 35),
+                  Center(
+                    child: Column(
+                      children: const [
+                        AddAndBuyButton(
+                            text: 'Add to Cart', color: Colors.blue),
+                        SizedBox(height: 15.0),
+                        AddAndBuyButton(text: 'Buy it Now', color: Colors.red),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AddAndBuyButton extends StatelessWidget {
+  const AddAndBuyButton({
+    Key? key,
+    required this.text,
+    required this.color,
+  }) : super(key: key);
+  final String text;
+  final Color color;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      width: 350,
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: Center(
+        child: Text(
+          text,
+          style: const TextStyle(
+            fontSize: 16,
+            color: Colors.white,
+            fontWeight: FontWeight.w500,
           ),
-        ],
+        ),
       ),
     );
   }
