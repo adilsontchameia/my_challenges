5360ae104a481cf41ca2897c2e3007a364e7f416
Adilson Tchameia <adilsonkchameia@outlook.com>
2022-12-01 17:01:39 +0100

    splash screen

--- ecommerce_ui/lib/src/pages/splash_screen/splash_screen.dart
+++ ecommerce_ui/lib/src/pages/splash_screen/splash_screen.dart
@@ -11,14 +11,60 @@ class SplashScreen extends StatelessWidget {
         child: Stack(
           children: [
             Align(
-              alignment: Alignment.center,
+              alignment: Alignment.topCenter,
               child: Opacity(
                 opacity: 0.5,
                 child: Image.asset(
                   fit: BoxFit.cover,
+                  height: 700,
                   'assets/breakfast.png',
                 ),
               ),
+            ),
+            Transform.rotate(
+              angle: -0.5,
+              alignment: Alignment.centerLeft,
+              child: Opacity(
+                opacity: 0.7,
+                child: Align(
+                  alignment: Alignment.topCenter,
+                  child: Image.asset(
+                    fit: BoxFit.cover,
+                    height: 590,
+                    'assets/headphone_red.png',
+                  ),
+                ),
+              ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'SOUND',
+                  style: TextStyle(
+                    fontSize: 80.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const Text(
+                  'SHOP',
+                  style: TextStyle(
+                    fontSize: 80.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'Loren Ipsun Loren Ipsun Loren Ipsun Loren Ipsun Loren IpsunLoren Ipsun Loren Ipsun',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    color: Colors.white.withOpacity(0.7),
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
             ),
             Align(
               alignment: Alignment.bottomCenter,
