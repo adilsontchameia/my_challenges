70bef786c99cce197e4544db7b47c6109ff3019b
Adilson Tchameia <adilsonkchameia@outlook.com>
2022-12-03 11:31:36 +0100

    changing componnents

--- ecommerce_ui/lib/src/models/home_card_model.dart
+++ ecommerce_ui/lib/src/models/home_card_model.dart
@@ -16,7 +16,7 @@ class ProductsModel {
         productName: 'JBL x90',
         productPrice: 90,
         productDescription: '',
-        imgUrl: 'assets/airpods.png',
+        imgUrl: 'assets/headphone_blue.png',
         backGroundColor: const Color.fromRGBO(4, 4, 5, 1)),
     ProductsModel(
         productName: 'JBL x90',
